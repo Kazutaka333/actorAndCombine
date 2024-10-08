6076c63642de253258b2cdb5f7f9537084dacbdd
Kazutaka Homma <kaz@flexfitnessapp.com>
2024-10-07 19:29:05 -0700

    Initial Commit

---
+++ actorAndCombine/actorAndCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  actorAndCombineApp.swift
+//  actorAndCombine
+//
+//  Created by Kazutaka Homma on 2024-10-07.
+//
+
+import SwiftUI
+
+@main
+struct actorAndCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
